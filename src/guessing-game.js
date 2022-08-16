9e97ac838c305c6980c8d14739d69b500b72b371
Leenok <34567431+Leenok@users.noreply.github.com>
2022-08-16 23:04:30 +0300

    init: add first solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
+        this.r = 0;
     }
 
     guess() {
-
+        this.r = Math.round(this.max+ this.min/2);
+        if(this.r <number){return this.greater()}
+        if(this.r >number){ return this.lower()};
+        if(this.r === number) {return this.r };
     }
 
     lower() {
-
+        this.max = this.r ;
     }
 
     greater() {
-
+         this.min = this.r ;
     }
 }
 
