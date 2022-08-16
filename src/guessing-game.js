13f96adf6390d3b8040442963cfbc1b98222b728
Leenok <34567431+Leenok@users.noreply.github.com>
2022-08-16 23:13:19 +0300

    init: atempt 2

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,17 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = null;
+        this.min = null;
+        this.r = null;
+    }
 
     setRange(min, max) {
         this.max = max;
         this.min = min;
-        this.r = 0;
     }
 
     guess() {
         this.r = Math.round(this.max+ this.min/2);
-        if(this.r <number){return this.greater()}
-        if(this.r >number){ return this.lower()};
-        if(this.r === number) {return this.r };
+        //if(this.r <number){return this.greater()}
+        //if(this.r >number){ return this.lower()};
+        //if(this.r === number) {return this.r };
+        return this.r;
     }
 
     lower() {
